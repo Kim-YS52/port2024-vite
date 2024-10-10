69cade5cb70f3e7a9f10591166dc7971b79223af
KimYS <withseven87@gmail.com>
2024-10-10 15:03:58 +0900

    포트폴리오 완료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,22 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 export function port(){
-    console.log(port)
+    gsap.registerPlugin(ScrollTrigger);
+    const horSection=gsap.utils.toArray(".port__item");
+    
+    gsap.to(horSection,{
+        xPercent:-120*(horSection.length-1),
+        ease:"none",
+        scrollTrigger: {
+           trigger:"#port",
+           start:"top 56px",
+           end:"+=3000",
+           scrub:1,
+           pin:true,
+           markers:false,
+           invalidateOnRefresh:true,
+           anticipatePin:1,
+        },
+    });
 }
